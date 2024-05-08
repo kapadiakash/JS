a97a805d7f9f142ae3a79085d51f0230ba55bed2
Akash kapadi <kapadiakash1@gmail.com>
2024-05-08 20:24:43 +0530

    update

---
+++ first.js
@@ -0,0 +1,17 @@
+name = 'Akash';
+console.log('akash');
+age = 23;
+price = 99.99;
+console.log(age);
+console.log(price);
+x = null;
+console.log(x);
+
+let fullName = 'Tony';
+fullName = 'akash'
+console.log(fullName)
+
+// console.log('Hii Hello');
+// console.log('Hii Shet');
+
+
